88ffffdb7093beba038989b441c31d1a84ef4734
Jose Moreno <jgmorenor277@gmail.com>
2025-05-13 19:32:07 -0500

    Primer commit con la instalación de react, tailwind y la creación de gitignore

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">¡Hola desde React con Tailwind!</h1>
+      <p>Este es un texto de prueba.</p>
+    </div>
+  );
+}
+
+export default App;
